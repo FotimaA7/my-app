8fbd7774ac7b5804f2312656703f898ddd0d4466
fotima.abdulmainova <abdulmainovafotima@gmail.com>
2024-12-06 13:55:17 +0600

    Add

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import UserList from './UserList';
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <UserList />
+    </div>
+  );
+}
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+export default App;
